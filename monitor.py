a911275ab84277868de99c53a322b34d5d702e1c
kyleotero <kkyleotero@gmail.com>
2022-03-14 19:29:18 -0400

    added ability to monitor multiple stores, as well as sending notifications for each new product.

--- monitor.py
+++ monitor.py
@@ -8,55 +8,81 @@ import discord
 from discord import Webhook, RequestsWebhookAdapter
 from bs4 import BeautifulSoup
 import time
-from PIL import Image
 import urllib.request
 
-client = discord.Client()
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-
-
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=chrome_options
-)
-driver.get(
-    "https://www.canadacomputers.com/index.php?cPath=43_557&sf=:3_3,3_4,3_5,3_6,3_7,3_8,3_9,3_12,3_29,3_30,3_31,3_32,3_33&loc=NMKT&mfr=&pr="
-)
-
-
-last_h = driver.execute_script("return document.body.scrollHeight")
+count = 0
+links = {
+    "Newmarket": "https://www.canadacomputers.com/index.php?cPath=43_557&sf=:3_3,3_4,3_5,3_6,3_7,3_8,3_9,3_12,3_29,3_30,3_31,3_32,3_33&loc=NMKT&mfr=&pr=",
+    "North York": "https://www.canadacomputers.com/index.php?cPath=43_557&sf=:3_3,3_4,3_5,3_6,3_7,3_8,3_9,3_12,3_29,3_30,3_31,3_32,3_33&loc=NO&mfr=&pr=",
+    "Brampton": "https://www.canadacomputers.com/index.php?cPath=43_557&sf=:3_3,3_4,3_5,3_6,3_7,3_8,3_9,3_12,3_29,3_30,3_31,3_32,3_33&loc=1304&mfr=&pr=",
+    "Etobicoke": "https://www.canadacomputers.com/index.php?cPath=43_557&sf=:3_3,3_4,3_5,3_6,3_7,3_8,3_9,3_12,3_29,3_30,3_31,3_32,3_33&loc=ETOB&mfr=&pr=",
+    "Markham": "https://www.canadacomputers.com/index.php?cPath=43_557&sf=:3_3,3_4,3_5,3_6,3_7,3_8,3_9,3_12,3_29,3_30,3_31,3_32,3_33&loc=MU&mfr=&pr=",
+    "Mississauga": "https://www.canadacomputers.com/index.php?cPath=43_557&sf=:3_3,3_4,3_5,3_6,3_7,3_8,3_9,3_12,3_29,3_30,3_31,3_32,3_33&loc=MISS&mfr=&pr=",
+}
+stores = ["Newmarket", "North York", "Brampton", "Etobicoke", "Markham", "Mississauga"]
+old = [[] for store in stores]
+new = [[] for store in stores]
+init = True
 while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(0.5)
-    new_h = driver.execute_script("return document.body.scrollHeight")
-    if new_h == last_h:
-        break
-    last_h = new_h
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=chrome_options
+    )
+    driver.get(links[stores[count]])
 
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
+    last_h = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.5)
+        new_h = driver.execute_script("return document.body.scrollHeight")
+        if new_h == last_h:
+            break
+        last_h = new_h
 
+    content = driver.page_source
+    soup = BeautifulSoup(content, "html.parser")
 
-channel = client.get_channel(828037920564183100)
-webhook = Webhook.from_url(
-    "https://discord.com/api/webhooks/828039746986442753/M-9VnM4P1huQrQXvaIlhj-_FHNjYKgzDtiWjkWQKRmRcUHFamhhsYG8CDY80o4SmIO-T",
-    adapter=RequestsWebhookAdapter(),
-)
-for element in soup.findAll(attrs={"class": "col-12 productImageDesc"}):
-    sub = element.findChildren(recursive="false")
-    for child in sub:
-        if child.find(attrs={"class": "text-dark text-truncate_3"}):
-            title = child.find(attrs={"class": "text-dark text-truncate_3"})
-        if child.find(attrs={"class": "d-block mb-0 pq-hdr-product_price line-height"}):
-            price = child.find(
+    webhook = Webhook.from_url(
+        "https://discord.com/api/webhooks/828039746986442753/M-9VnM4P1huQrQXvaIlhj-_FHNjYKgzDtiWjkWQKRmRcUHFamhhsYG8CDY80o4SmIO-T",
+        adapter=RequestsWebhookAdapter(),
+    )
+    for element in soup.findAll(attrs={"class": "col-12 productImageDesc"}):
+        sub = element.findChildren(recursive="false")
+        for child in sub:
+            if child.find(attrs={"class": "text-dark text-truncate_3"}):
+                title = child.find(attrs={"class": "text-dark text-truncate_3"})
+            if child.find(
                 attrs={"class": "d-block mb-0 pq-hdr-product_price line-height"}
-            )
-        if child.find(attrs={"class": "pq-img-manu_logo align-self-center"}):
-            image = child.find(attrs={"class": "pq-img-manu_logo align-self-center"})
-    urllib.request.urlretrieve(image["src"], "gpu.jpg")
-    img = discord.File("./gpu.jpg", filename="gpu.jpg")
-    embed = discord.Embed(title="New card found", url=title["href"])
-    embed.set_thumbnail(url="attachment://gpu.jpg")
-    embed.add_field(name="Product:", value=title.text)
-    embed.add_field(name="Price:", value=price.text)
-    webhook.send(file=img, embed=embed)
+            ):
+                price = child.find(
+                    attrs={"class": "d-block mb-0 pq-hdr-product_price line-height"}
+                )
+            if child.find(attrs={"class": "pq-img-manu_logo align-self-center"}):
+                image = child.find(
+                    attrs={"class": "pq-img-manu_logo align-self-center"}
+                )
+        if init == True:
+            old[count].append(title.text)
+            new[count].append(title.text)
+        else:
+            new[count].append(title.text)
+            if title.text not in old[count]:
+                urllib.request.urlretrieve(image["src"], "gpu.jpg")
+                img = discord.File("./gpu.jpg", filename="gpu.jpg")
+                embed = discord.Embed(title="New card found", url=title["href"])
+                embed.set_thumbnail(url="attachment://gpu.jpg")
+                embed.add_field(name="Product:", value=title.text)
+                embed.add_field(name="Price:", value=price.text)
+                embed.add_field(name="Store", value=stores[count])
+                webhook.send(file=img, embed=embed)
+    if init == False:
+        old[count] = new[count]
+        new[count] = []
+
+    if count < len(stores) - 1:
+        print(old[count])
+        count += 1
+    else:
+        count = 0
+        init = False
